6c6e1c0538773d5b7dfaed89287ba08752106dbf
kirill <shkolenko02-03-2004@mail.ru>
2024-04-13 12:06:10 +0300

    Added the errors handling

---
+++ errors/errors.py
@@ -0,0 +1,20 @@
+class ErrEntityNotFound(Exception):
+    def __int__(self, message):
+        super().__init__(message)
+
+
+class ErrEntityConflict(Exception):
+    def __int__(self, message):
+        super().__init__(message)
+
+
+class ErrBadRequest(Exception):
+    def __int__(self, message):
+        super().__init__(message)
+
+
+class ErrNotAuthorized(Exception):
+    def __int__(self, message):
+        super().__init__(message)
+
+
--- utils/errors.py
+++
@@ -1,8 +0,0 @@
-class ErrEntityNotFound(Exception):
-    def __int__(self, message):
-        super().__init__(message)
-
-
-class ErrEntityConflict(Exception):
-    def __int__(self, message):
-        super().__init__(message)
